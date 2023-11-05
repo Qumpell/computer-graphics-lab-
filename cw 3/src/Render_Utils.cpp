c3ed2ffcc7c94edd6df59a521a2f71dfe18c8672
Qumpell <qqumpel@gmail.com>
2023-11-05 23:11:39 +0100

    Add solution for homework

--- cw 3/src/Render_Utils.cpp
+++ cw 3/src/Render_Utils.cpp
@@ -15,17 +15,22 @@ void Core::RenderContext::initFromAssimpMesh(aiMesh* mesh) {
     vertexBuffer = 0;
     vertexIndexBuffer = 0;
 
-    std::vector<aiVector2D> textureCoord;
+    //std::vector<aiVector2D> textureCoord;
+    std::vector<float> textureCoord;
     std::vector<unsigned int> indices;
     //tex coord must be converted to 2d vecs
     for (unsigned int i = 0; i < mesh->mNumVertices; i++)
     {
         if (mesh->mTextureCoords[0] != nullptr) {
             //aiVector2D()
-            textureCoord.push_back(aiVector2D(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y));
+           // textureCoord.push_back(aiVector2D(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y));
+            textureCoord.push_back(mesh->mTextureCoords[0][i].x);
+            textureCoord.push_back(mesh->mTextureCoords[0][i].y);
         }
         else {
-            textureCoord.push_back(aiVector2D(0.0f, 0.0f));
+            //textureCoord.push_back(aiVector2D(0.0f, 0.0f));
+            textureCoord.push_back(0.0f);
+            textureCoord.push_back(0.0f);
         }
     }
     if (mesh->mTextureCoords[0] == nullptr) {
@@ -39,6 +44,50 @@ void Core::RenderContext::initFromAssimpMesh(aiMesh* mesh) {
             indices.push_back(face.mIndices[j]);
     }
 
+    unsigned int vertexDataBufferSize = sizeof(float) * mesh->mNumVertices * 3;
+    unsigned int vertexNormalBufferSize = sizeof(float) * mesh->mNumVertices * 3;
+    unsigned int vertexTexBufferSize = sizeof(float) * mesh->mNumVertices * 2;
+    //unsigned int vertexTangentBufferSize = sizeof(float) * mesh->mNumVertices * 3;
+    //unsigned int vertexBiTangentBufferSize = sizeof(float) * mesh->mNumVertices * 3;
+
+    unsigned int vertexElementBufferSize = sizeof(unsigned int) * indices.size();
+    size = indices.size();
+
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+
+    glGenBuffers(1, &vertexIndexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, vertexElementBufferSize, &indices[0], GL_STATIC_DRAW);
+
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    //std::cout << vertexBuffer;
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    //glEnableVertexAttribArray(3);
+    //glEnableVertexAttribArray(4);
+
+    //glBufferData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize + vertexTangentBufferSize + //vertexBiTangentBufferSize, NULL, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize, NULL, GL_STATIC_DRAW);
+
+    glBufferSubData(GL_ARRAY_BUFFER, 0, vertexDataBufferSize, mesh->mVertices);
+
+    glBufferSubData(GL_ARRAY_BUFFER, vertexDataBufferSize, vertexNormalBufferSize, mesh->mNormals);
+
+    glBufferSubData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize, vertexTexBufferSize, &textureCoord[0]);
+
+    //glBufferSubData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize, vertexTangentBufferSize, mesh->mTangents);
+
+    //glBufferSubData(GL_ARRAY_BUFFER, vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize + vertexTangentBufferSize, vertexBiTangentBufferSize, mesh->mBitangents);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)(0));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)(vertexDataBufferSize));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (void*)(vertexNormalBufferSize + vertexDataBufferSize));
+    //glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, (void*)(vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize));
+    //glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 0, (void*)(vertexDataBufferSize + vertexNormalBufferSize + vertexTexBufferSize + vertexTangentBufferSize));
 
 
 
--- cw 3/src/ex_3_1.hpp
+++ cw 3/src/ex_3_1.hpp
@@ -25,36 +25,41 @@ Core::RenderContext sphereContext;
 
 glm::vec3 cameraPos = glm::vec3(-1.0f, 0.0f, 0.0f);
 glm::vec3 cameraDir = glm::vec3(1.f, 0.f, 0.f);
+glm::vec3 p = glm::vec3(0.0f, 0.0f, 0.0f);
 GLuint VAO,VBO;
 
 float aspectRatio = 1.f;
 
 
 glm::mat4 createCameraMatrix(){
-
 	glm::vec3 cameraSide = glm::normalize(glm::cross(cameraDir, glm::vec3(0.f, 1.f, 0.f)));
 	glm::vec3 cameraUp = glm::normalize(glm::cross(cameraSide, cameraDir));
-	
+
+
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// ! Macierz translation jest definiowana wierszowo dla poprawy czytelnosci. OpenGL i GLM domyslnie stosuje macierze kolumnowe, dlatego musimy ja transponowac !
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// ! Macierz translation jest definiowana wierszowo dla poprawy czytelnosci. OpenGL i GLM domyslnie stosuje macierze kolumnowe, dlatego musimy ja transponowac !
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
 	glm::mat4 cameraRotationMatrix = glm::mat4({
 		cameraSide.x, cameraSide.y, cameraSide.z, 0.,
 		cameraUp.x, cameraUp.y, cameraUp.z, 0.,
 		-cameraDir.x, -cameraDir.y, -cameraDir.z,0.,
 		0., 0., 0., 1.,
-		});
-	glm::mat4 translationMatrix = glm::mat4({
+	});
+	cameraRotationMatrix = glm::transpose(cameraRotationMatrix);
+
+	glm::mat4 cameraTranslationMatrix = glm::mat4({
 		1.,0.,0.,-cameraPos.x,
 		0.,1.,0.,-cameraPos.y,
 		0.,0.,1.,-cameraPos.z,
 		0.,0.,0.,1.,
 	});
-	
-	cameraRotationMatrix = glm::transpose(cameraRotationMatrix * glm::translate(cameraRotationMatrix, -cameraPos));
-	glm::mat4 cameraMatrix = cameraRotationMatrix;
+	cameraTranslationMatrix = glm::transpose(cameraTranslationMatrix);
+	glm::mat4 cameraMatrix = cameraRotationMatrix * cameraTranslationMatrix;
 
 	return cameraMatrix;
+
 }
 
 glm::mat4 createPerspectiveMatrix(float fov, float aspectRatio){
@@ -64,7 +69,7 @@ glm::mat4 createPerspectiveMatrix(float fov, float aspectRatio){
 	// ! Macierz translation jest definiowana wierszowo dla poprawy czytelnosci. OpenGL i GLM domyslnie stosuje macierze kolumnowe, dlatego musimy ja transponowac !
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	float n = 0.1f;
-	float f = 10.0f;
+	float f = 100.0f;
 
 	float S = 1.0f / tan((fov/2.0f) * M_PI/180.0f);
 	//printf("%f\n", aspectRatio);
@@ -78,7 +83,24 @@ glm::mat4 createPerspectiveMatrix(float fov, float aspectRatio){
 	
 	perspectiveMatrix=glm::transpose(perspectiveMatrix);
 
-	return perspectiveMatrix;
+	//return perspectiveMatrix;
+	return glm::perspective(glm::radians(fov), aspectRatio, n, f);
+}
+glm::mat4 createTransformationMatrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale) {
+	glm::mat4 transformationMatrix = glm::mat4(1.0f);
+
+	// Translacja
+	transformationMatrix = glm::translate(transformationMatrix, translation);
+
+	// Rotacja
+	transformationMatrix = glm::rotate(transformationMatrix, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	transformationMatrix = glm::rotate(transformationMatrix, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	transformationMatrix = glm::rotate(transformationMatrix, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+
+	// Skala
+	transformationMatrix = glm::scale(transformationMatrix, scale);
+
+	return transformationMatrix;
 }
 void renderScene(GLFWwindow* window)
 {
@@ -89,12 +111,38 @@ void renderScene(GLFWwindow* window)
 
 	glUseProgram(program_box);
 	
-	float fov = 90.0f;
+	float fov = 45.0f;
+
+
 
 	glBindVertexArray(VAO);
-	transformation = createCameraMatrix() * createPerspectiveMatrix(fov, aspectRatio);
+	transformation = createPerspectiveMatrix(fov, aspectRatio) * createCameraMatrix();
 	glUniformMatrix4fv(glGetUniformLocation(program_box, "transformation"), 1, GL_FALSE, (float*)&transformation);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//glDrawArrays(GL_TRIANGLES, 0, 36);
+	Core::DrawContext(sphereContext);
+
+
+	glm::vec3 translation = glm::vec3(-1.0f, -0.5f, 0.0f);
+	glm::vec3 rotation = glm::vec3(0.0f, 60.0f, 0.0f);
+	glm::vec3 scale = glm::vec3(0.5f, 0.5f, 0.5f);
+	
+	// Translacja
+	glm::mat4 transformationMatrix = createTransformationMatrix(translation, rotation, scale);
+	glm::mat4 transformation1 = transformationMatrix * createPerspectiveMatrix(fov, aspectRatio)  * createCameraMatrix();
+	glUniformMatrix4fv(glGetUniformLocation(program_box, "transformation"), 1, GL_FALSE, (float*)&transformation1);
+	//glDrawArrays(GL_TRIANGLES, 0, 36);
+	Core::DrawContext(shipContext);
+
+	glm::vec3 translation1 = glm::vec3(0.5f, -1.0f, 0.0f);
+	glm::vec3 rotation1 = glm::vec3(0.0f, 70.0f, 0.0f);
+	glm::vec3 scale1 = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 transformationMatrix1 = createTransformationMatrix(translation1, rotation1, scale1);
+	glm::mat4 transformation2 = transformationMatrix1 * createPerspectiveMatrix(fov, aspectRatio) * createCameraMatrix();
+	//glUniformMatrix4fv(glGetUniformLocation(program_box, "transformation"), 1, GL_FALSE, (float*)&transformation2);
+	//glDrawArrays(GL_TRIANGLES, 0, 36);
+	
+
+
 	glBindVertexArray(0);
 
 	glUseProgram(0);
@@ -118,6 +166,12 @@ void loadModelToContext(std::string path, Core::RenderContext& context)
 		std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
 		return;
 	}
+	
+    std::vector<float> combinedData; // Zbiorcza tablica dla wierzchołków, normalnych i współrzędnych tekstur
+    std::vector<unsigned int> indices;
+
+	context.initFromAssimpMesh(scene->mMeshes[0]);
+	
 }
 
 void init(GLFWwindow* window)
@@ -127,6 +181,11 @@ void init(GLFWwindow* window)
 	glEnable(GL_DEPTH_TEST);
 	program = shaderLoader.CreateProgram("shaders/shader_3_1.vert", "shaders/shader_3_1.frag");
 	program_box = shaderLoader.CreateProgram("shaders/shader_2_1.vert", "shaders/shader_2_1.frag");
+
+	loadModelToContext("./models/sphere.obj", sphereContext);
+	loadModelToContext("./models/spaceship.obj", shipContext);
+
+	
 
 	glGenVertexArrays(1, &VAO);
 	// 2. Powiąż stworzone VAO za pomocą funkcji glBindVertexArray
@@ -145,6 +204,7 @@ void init(GLFWwindow* window)
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(4 * sizeof(float)));
 
+
 }
 
 void shutdown(GLFWwindow* window)
@@ -156,7 +216,9 @@ void shutdown(GLFWwindow* window)
 void processInput(GLFWwindow* window)
 {
 	glm::vec3 cameraSide = glm::normalize(glm::cross(cameraDir, glm::vec3(0.f,1.f,0.f)));
-	//cameraSide = glm::normalize(glm::cross(cameraDir, glm::vec3(0.f, 1.f, 0.f)));
+
+
+
 	float angleSpeed = 0.05f;
 	float moveSpeed = 0.05f;
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -170,11 +232,18 @@ void processInput(GLFWwindow* window)
 		cameraPos -= cameraSide * moveSpeed;
 	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
 		cameraPos += cameraSide * moveSpeed;
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		cameraDir = glm::vec3(glm::eulerAngleY(angleSpeed) * glm::vec4(cameraDir, 0));
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		cameraDir = glm::vec3(glm::eulerAngleY(-angleSpeed) * glm::vec4(cameraDir, 0));
-
+	//if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		//cameraDir = glm::vec3(glm::eulerAngleY(angleSpeed) * glm::vec4(cameraDir, 0));
+	//if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		//cameraDir = glm::vec3(glm::eulerAngleY(-angleSpeed) * glm::vec4(cameraDir, 0));
+	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+		p.y += moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+		p.y -= moveSpeed;
+	
+	
+	
+	cameraDir = glm::normalize(p - cameraPos);
 }
 
 // funkcja jest glowna petla
