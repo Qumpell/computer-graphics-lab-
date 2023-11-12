ed6c4e1b085133aa00622f61373afdca11966b16
Qumpell <qqumpel@gmail.com>
2023-11-12 22:28:25 +0100

    Add solutions to lab5

--- cw 4/shaders/shader_4_1.frag
+++ cw 4/shaders/shader_4_1.frag
@@ -2,9 +2,35 @@
 uniform vec3 color;
 out vec4 out_color;
 
+float linearizeDepth(float depth, float near, float far) {
+    float z = depth * 2.0 - 1.0;  // Znormalizuj zakres [0, 1] do [-1, 1]
+    return (2.0 * near * far) / (far + near - z * (far - near));
+}
 
 void main()
 {
-	//out_color = vec4(1,0,1,1);
-	out_color = vec4(color.x,color.y,color.z,1);
+	//out_color = vec4(gl_FragCoord.z,gl_FragCoord.z,gl_FragCoord.z,1);
+	 // Zakres odleg³oœci kamery od fragmentu (liniowe)
+    float linearDistance = linearizeDepth(gl_FragCoord.z, 0.1, 1.0);
+	  // Kolor zale¿ny od odleg³oœci
+    //vec3 color = vec3(1.0, 0.0, 0.0) * linearDistance;
+    //out_color = vec4(color, 1.0);
+    
+    
+	//out_color = vec4(color.x,color.y,color.z,1);
+
+
+    
+    // Kolor t³a
+    vec3 backgroundColor = vec3(0.0f, 0.3f, 0.3f);
+
+    // Wspó³czynnik mg³y (mo¿esz dostosowaæ wartoœæ dla po¿¹danego efektu)
+    float fogFactor = linearDistance;
+
+    // Zastosuj efekt mg³y mieszaj¹c kolory obiektu i t³a
+    vec3 finalColor = mix(color, backgroundColor, fogFactor);
+
+    out_color = vec4(finalColor, 1.0);
+
+    out_color = vec4(color.x,color.y,color.z,1);
 }
--- cw 4/src/ex_4_1.hpp
+++ cw 4/src/ex_4_1.hpp
@@ -22,9 +22,20 @@ Core::Shader_Loader shaderLoader;
 
 Core::RenderContext shipContext;
 Core::RenderContext sphereContext;
+Core::RenderContext asteroidContext1;
+Core::RenderContext asteroidContext2;
 
+//glm::vec3 spaceshipPos;
+//glm::vec3 spaceshipDir;
 glm::vec3 cameraPos = glm::vec3(-4.f, 0, 0);
 glm::vec3 cameraDir = glm::vec3(1.f, 0.f, 0.f);
+glm::vec3 spaceshipPos = cameraPos + 1.5f * cameraDir + glm::vec3(0, -0.5f, 0);
+glm::vec3 spaceshipDir = cameraDir;
+float lastFrameTime;
+float deltaTime;
+
+
+
 GLuint VAO,VBO;
 
 float aspectRatio = 1.f;
@@ -89,52 +100,145 @@ void drawObjectColor(Core::RenderContext& context, glm::mat4 modelMatrix, glm::v
 	glUniformMatrix4fv(glGetUniformLocation(program, "transformation"), 1, GL_FALSE, (float*)&transformation);
 	Core::DrawContext(context);
 }
-void renderScene(GLFWwindow* window)
-{
-	glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glm::mat4 transformation;
+void drawPlanet(Core::RenderContext& context, const glm::mat4& parentMatrix, float radius, const glm::vec3& color, float rotationSpeed, float distance, bool is_with_moon) {
 	float time = glfwGetTime();
-	glm::vec3 rotationVector(0.0f, 1.0f, 1.0f);
-	//float angle = sin(time) * 2.0f;
-	glUseProgram(program);
-
-
-	// Promień obiektu
-	float radius = 2.0f;
 
 	// Kąt obrotu
-	float angle = glm::radians(glfwGetTime() * 45.0f); // Przykładowa prędkość obrotu
-
-	// Ustawienie położenia pierwszego obiektu
-	glm::mat4 modelMatrix1 = glm::translate(glm::mat4(1.0f), glm::vec3(3.0f, 0.0f, 0.0f));
-	modelMatrix1 = glm::rotate(modelMatrix1, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f)); // Obrót wokół osi Y
-
-	// Ustawienie położenia i rotacji drugiego obiektu wokół pierwszego
-	glm::mat4 modelMatrix2 = glm::translate(glm::mat4(1.0f), glm::vec3(3.0f + angle, -1.0f + angle, -1.0f + angle)); // Dystans od pierwszego obiektu
-
-	// Punkt obrotu
-	glm::vec3 rotationPoint(0.0f, 0.0f, 0.0f);
+	float rotationAngle = glm::radians(time * rotationSpeed);
 
 
-	// Wyznacz nową pozycję na okręgu
+	glm::vec3 planetPosition(0.0f, 0.0f, 0.0f);
+	// Wyznacz nową pozycję na okręgu dla planety
 	glm::vec3 newPosition;
-	newPosition.x = rotationPoint.x + radius * cos(angle);
-	newPosition.y = rotationPoint.y;
-	newPosition.z = rotationPoint.z + radius * sin(angle);
+	newPosition.x = planetPosition.x;
+	newPosition.y = planetPosition.y + distance * cos(rotationAngle);
+	newPosition.z = planetPosition.z + distance * sin(rotationAngle);
 
-	// Utwórz macierz translacji
-	glm::mat4 translationMatrix = glm::translate(modelMatrix1, newPosition);
-	// Ustawienie modelMatrix w funkcji drawObjectColor
-	drawObjectColor(sphereContext, modelMatrix1, glm::vec3(0.5, 0.3, 0.0));
-	drawObjectColor(sphereContext, translationMatrix, glm::vec3(0.5, 0.2, 4.0));
+	// Macierz modelu planety
+	glm::mat4 planetMatrix = glm::scale(glm::translate(parentMatrix, newPosition), glm::vec3(radius));
 
-	translationMatrix = glm::scale( glm::translate(translationMatrix, newPosition), glm::vec3(0.5f, 0.5f, 0.5f));
-	drawObjectColor(sphereContext, translationMatrix, glm::vec3(0.5, 0.2, 4.0));
+	// Narysuj planetę
+	drawObjectColor(context, planetMatrix, color);
 
-	//drawObjectColor(sphereContext, glm::rotate(glm::translate(glm::vec3(0, angle, 0)), angle, rotationVector), glm::vec3(0.9, 0.9, 0.2));
+	if (is_with_moon) {
+		drawPlanet(context, planetMatrix, 0.2f, glm::vec3(0.3f, 0.0f, 1.0f), 80.0f, 1.4f, false);
+	}
 
-	//drawObjectColor(sphereContext, glm::translate(glm::vec3(0, 0, 1)), glm::vec3(0.5, 0.3, 0.));
+}
+void drawAsteroidBelt(Core::RenderContext& context, const glm::mat4& sunMatrix, int count, float innerRadius, float outerRadius) {
+	float time = glfwGetTime();
+	float speed = 1.0f;
+	for (int i = 0; i < count; ++i) {
+		float angle = glm::radians(time * speed * i);  // Przykładowa prędkość obrotu asteroidy
+
+		// Pozycja asteroidy na pasie
+		float radius = glm::mix(innerRadius, outerRadius, glm::sin(angle * 2.0f));  // Użyj funkcji sinus, aby uzyskać efekt falowania
+		glm::vec3 asteroidPosition(
+			0.0f,
+			radius * sin(angle),
+			radius * cos(angle)
+		);
+
+		// Macierz modelu asteroidy
+		glm::mat4 asteroidMatrix = glm::scale(glm::translate(sunMatrix, asteroidPosition), glm::vec3(0.001f));  // Skaluj asteroidy według potrzeb
+
+		// Narysuj asteroidę
+		drawObjectColor(context, asteroidMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
+	}
+}
+
+void drawSolarSystem(Core::RenderContext& context) {
+	float time = glfwGetTime();
+
+	// Narysuj słońce
+	drawObjectColor(context, glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+
+	// Narysuj planety
+	drawPlanet(context, glm::mat4(1.0f), 0.5f, glm::vec3(0.0f, 0.0f, 1.0f), 45.0f, 1.9f, true);
+	drawPlanet(context, glm::mat4(1.0f), 0.4f, glm::vec3(0.5f, 0.0f, 0.5f), 50.0f, 3.1f, false);
+	drawPlanet(context, glm::mat4(1.0f), 0.3f, glm::vec3(0.0f, 0.5f, 0.0f), 55.0f, 4.5f, false);
+	drawPlanet(context, glm::mat4(1.0f), 0.2f, glm::vec3(0.8f, 0.2f, 0.2f), 60.0f, 6.0f, false);
+	drawPlanet(context, glm::mat4(1.0f), 0.15f, glm::vec3(0.2f, 0.2f, 0.8f), 65.0f, 7.5f, false);
+
+	// Narysuj pas asteroid
+	//drawAsteroidBelt(asteroidContext1, glm::mat4(1.0f), 100, 12.0f, 14.0f);
+	drawAsteroidBelt(asteroidContext1, glm::mat4(1.0f), 100, 13.0f, 15.0f);
+	drawAsteroidBelt(asteroidContext2, glm::mat4(1.0f), 100, 15.0f, 17.0f);
+}
+
+glm::mat4 createSpaceshipRotationMatrix()
+{
+	glm::vec3 cameraSide = glm::normalize(glm::cross(spaceshipDir, glm::vec3(0.f, 1.f, 0.f)));
+	glm::vec3 cameraUp = glm::normalize(glm::cross(cameraSide, spaceshipDir));
+	//glm::vec3 cameraUp = glm::vec3(0.f, 1.f, 0.f);
+	glm::mat4 cameraRotationMatrix = glm::mat4({
+		cameraSide.x,cameraSide.y,cameraSide.z,0,
+		cameraUp.x,cameraUp.y,cameraUp.z ,0,
+		-spaceshipDir.x,-spaceshipDir.y,-spaceshipDir.z,0,
+		0.,0.,0.,1.,
+		});
+
+
+
+	cameraRotationMatrix = glm::transpose(cameraRotationMatrix);
+
+	//glm::mat4 cameraMatrix = cameraRotationMatrix * glm::translate(-cameraPos);
+
+	return cameraRotationMatrix;
+}
+glm::mat4 createSpaceshipMatrix()
+{
+	// Ustal wektor w górę (załóżmy, że oś Y)
+	glm::vec3 spaceshipUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	// Utwórz macierz rotacji, która obraca statek w kierunku, w którym się porusza
+	glm::mat4 spaceshipRotationMatrix = glm::lookAt(glm::vec3(0.0f), spaceshipDir, spaceshipUp);
+
+	// Utwórz macierz translacji dla pozycji statku
+	glm::mat4 spaceshipTranslationMatrix = glm::translate(glm::mat4(1.0f), spaceshipPos);
+
+	// Połącz obie macierze, aby uzyskać macierz modelu statku
+	glm::mat4 spaceshipModelMatrix = spaceshipTranslationMatrix * spaceshipRotationMatrix;
+
+	return spaceshipModelMatrix;
+}
+void renderScene(GLFWwindow* window)
+{
+	
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(program);
+	glDisable(GL_DEPTH_TEST);
+	drawObjectColor(sphereContext, glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(4.f, 3.f, 5.f)), glm::vec3(0.03f)), glm::vec3(1.0f, 0.9f, 0.2f));
+	drawObjectColor(sphereContext, glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(7.f, 4.f, 2.f)), glm::vec3(0.03f)), glm::vec3(1.0f, 0.9f, 0.2f));
+	drawObjectColor(sphereContext, glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, -3.f, -2.f)), glm::vec3(0.03f)), glm::vec3(1.0f, 0.9f, 0.2f));
+	drawObjectColor(sphereContext, glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 5.f, 3.f)), glm::vec3(0.03f)), glm::vec3(1.0f, 0.9f, 0.2f));
+	drawObjectColor(sphereContext, glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-3.f, -3.f, -4.f)), glm::vec3(0.03f)), glm::vec3(1.0f, 0.9f, 0.2f));
+	drawObjectColor(sphereContext, glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 5.f)), glm::vec3(0.03f)), glm::vec3(1.0f, 0.9f, 0.2f));
+	glEnable(GL_DEPTH_TEST);
+
+
+
+
+	glm::mat4 transformation;
+	float time = glfwGetTime();
+
+	deltaTime = time - lastFrameTime;
+	lastFrameTime = time;
+	// Ucięcie deltaTime od góry do 0.1
+	deltaTime = glm::min(deltaTime, 0.1f);
+
+	glUseProgram(program);
+	
+	drawSolarSystem(sphereContext);
+
+	glm::mat4 spaceshipTranslationMatrix = glm::translate(createSpaceshipRotationMatrix(), spaceshipPos);
+	//drawObjectColor(shipContext,spaceshipTranslationMatrix, glm::vec3(1.0f, 0.0f, 0.0f));
+	//drawObjectColor(shipContext, glm::inverse(createSpaceshipRotationMatrix() * spaceshipTranslationMatrix), glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 spaceshipModelMatrix = createSpaceshipMatrix();
+	drawObjectColor(shipContext, spaceshipModelMatrix, glm::vec3(1.0f, 0.0f, 0.0f));
+	//drawObjectColor(asteroidContext1, glm::scale(spaceshipModelMatrix, glm::vec3(0.0005f)), glm::vec3(1.0f, 0.0f, 0.0f));
 
 	glUseProgram(0);
 	glfwSwapBuffers(window);
@@ -165,7 +269,9 @@ void init(GLFWwindow* window)
 	program = shaderLoader.CreateProgram("shaders/shader_4_1.vert", "shaders/shader_4_1.frag");
 
 	loadModelToContext("./models/sphere.obj",sphereContext);
-
+	loadModelToContext("./models/asteroid.obj", asteroidContext1);
+	loadModelToContext("./models/asteroid2.obj", asteroidContext2);
+	loadModelToContext("./models/spaceship.obj",shipContext);
 }
 
 void shutdown(GLFWwindow* window)
@@ -179,9 +285,14 @@ void processInput(GLFWwindow* window)
 	glm::vec3 cameraSide = glm::normalize(glm::cross(cameraDir, glm::vec3(0.f,1.f,0.f)));
 	float angleSpeed = 0.05f;
 	float moveSpeed = 0.05f;
+
+
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+
+
+
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 		cameraPos += cameraDir * moveSpeed;
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -195,8 +306,33 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraDir = glm::vec3(glm::eulerAngleY(-angleSpeed) * glm::vec4(cameraDir, 0));
 
-	//cameraDir = glm::normalize(-cameraPos);
 
+	// Poruszanie statkiem
+	float spaceshipSpeed = 5.0f;
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		spaceshipPos += spaceshipDir * spaceshipSpeed * deltaTime;;  // Przesunięcie do przodu
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		spaceshipPos -= spaceshipDir * spaceshipSpeed * deltaTime; ;  // Przesunięcie do tyłu
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+		//spaceshipDir = glm::rotateY(spaceshipDir, glm::radians(5.0f));  // Obrót w lewo
+		//spaceshipDir = glm::vec3(glm::eulerAngleY(angleSpeed) * glm::vec4(spaceshipDir, 0)) * deltaTime;;
+		spaceshipPos -= glm::normalize(glm::cross(spaceshipDir, glm::vec3(0.0f, 1.0f, 0.0f))) * spaceshipSpeed * deltaTime;
+	}
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+		//spaceshipDir = glm::rotateY(spaceshipDir, glm::radians(-5.0f));  // Obrót w prawo
+		//spaceshipDir = glm::vec3(glm::eulerAngleY(-angleSpeed) * glm::vec4(spaceshipDir, 0)) * deltaTime;
+		spaceshipPos += glm::normalize(glm::cross(spaceshipDir, glm::vec3(0.0f, 1.0f, 0.0f))) * spaceshipSpeed * deltaTime;
+	}
+
+
+
+	// Aktualizacja kamery
+	//cameraPos = spaceshipPos + 1.5f * spaceshipDir + glm::vec3(0, -0.5f, 0);
+	//cameraDir = spaceshipDir;
+	cameraPos = spaceshipPos + 1.5f * spaceshipDir + glm::vec3(0.0f, -0.5f, 0.0f);
+	cameraDir = -spaceshipDir;
 }
 
 // funkcja jest glowna petla
